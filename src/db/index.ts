a1206c2fd1a37599ee75a49ff13a6e28af915083
moinkhanp <moinkhanp1999@gmail.com>
2023-10-28 16:15:40 +0530

    add url into trpc

--- src/db/index.ts
+++ src/db/index.ts
@@ -5,12 +5,12 @@ declare global {
   var cachedPrisma: PrismaClient
 }
 
-let prisma: PrismaClient
+let prisma: PrismaClient  
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient()
 } else {
   if (!global.cachedPrisma) {
-    global.cachedPrisma = new PrismaClient()
+    global.cachedPrisma = new PrismaClient() 
   }
   prisma = global.cachedPrisma
 }
