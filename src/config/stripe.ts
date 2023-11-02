e3cb82226edc1a199bdf55998796eb64724901ee
moinkhanp <moinkhanp1999@gmail.com>
2023-11-02 18:05:55 +0530

    update page size

--- src/config/stripe.ts
+++ src/config/stripe.ts
@@ -18,7 +18,7 @@ export const PLANS = [
         name: "PRO",
         slug: "pro",
         quota: 50,
-        pagesPerPdf: 25,
+        pagesPerPdf: 400,
         price: {
             amount: 1190,
             priceIds:{
