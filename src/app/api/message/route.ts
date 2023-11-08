70c4579a38812af72615981885461550c905429a
moinkhanp <moinkhanp1999@gmail.com>
2023-11-08 18:25:44 +0530

    add set timeout

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -5,8 +5,9 @@ import { SendMessageValidator } from "@/lib/validators/SendMessageValidator";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import { PineconeStore } from "langchain/vectorstores/pinecone";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { OpenAIStream, StreamingTextResponse } from 'ai'
+import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export const POST = async (req: NextRequest) => {
     //endpoint for asking a question to a pdf file
@@ -73,53 +74,58 @@ export const POST = async (req: NextRequest) => {
         content: msg.text
     }))
 
-    const response = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
-        temperature: 0,
-        stream: true,
-        messages: [
-            {
-                role: 'system',
-                content:
-                    'Use the following pieces of context (or previous conversaton if needed) to answer the users question in markdown format.',
-            },
-            {
-                role: 'user',
-                content: `Use the following pieces of context (or previous conversaton if needed) to answer the users question in markdown format. \nIf you don't know the answer, just say that you don't know, don't try to make up an answer.
-              
-        \n----------------\n
-        
-        PREVIOUS CONVERSATION:
-        ${formattedPrevMessages.map((message) => {
-                    if (message.role === 'user')
-                        return `User: ${message.content}\n`
-                    return `Assistant: ${message.content}\n`
-                })}
-        
-        \n----------------\n
-        
-        CONTEXT:
-        ${results.map((r) => r.pageContent).join('\n\n')}
-        
-        USER INPUT: ${message}`,
-            },
-        ],
-    })
-
-    const stream = OpenAIStream(response, {
-        async onCompletion(completion) {
-            await db.message.create({
-                data: {
-                    text: completion,
-                    isUserMessage: false,
-                    fileId,
-                    userId,
+    setTimeout(async () => {
+        const response = await openai.chat.completions.create({
+            model: "gpt-3.5-turbo",
+            temperature: 0,
+            stream: true,
+            messages: [
+                {
+                    role: 'system',
+                    content:
+                        'Use the following pieces of context (or previous conversaton if needed) to answer the users question in markdown format.',
                 },
-            })
-        },
-    })
+                {
+                    role: 'user',
+                    content: `Use the following pieces of context (or previous conversaton if needed) to answer the users question in markdown format. \nIf you don't know the answer, just say that you don't know, don't try to make up an answer.
+                  
+            \n----------------\n
+            
+            PREVIOUS CONVERSATION:
+            ${formattedPrevMessages.map((message) => {
+                        if (message.role === 'user')
+                            return `User: ${message.content}\n`
+                        return `Assistant: ${message.content}\n`
+                    })}
+            
+            \n----------------\n
+            
+            CONTEXT:
+            ${results.map((r) => r.pageContent).join('\n\n')}
+            
+            USER INPUT: ${message}`,
+                },
+            ],
+        })
 
-    return new StreamingTextResponse(stream)
+        const stream = OpenAIStream(response, {
+            async onCompletion(completion) {
+                await db.message.create({
+                    data: {
+                        text: completion,
+                        isUserMessage: false,
+                        fileId,
+                        userId,
+                    },
+                })
+            },
+        })
+
+        return new StreamingTextResponse(stream)
+
+    }, 30000);
+
 
 
 }
+
