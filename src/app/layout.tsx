b1f167c82d6e653ac612e6568f7207c38851d044
moinkhanp <moinkhanp1999@gmail.com>
2025-10-01 16:16:31 +0530

    change in layout page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,12 @@
 import Navbar from '@/components/Navbar'
 import './globals.css'
 import { cn, constructMetadata } from '@/lib/utils'
-import { Inter } from 'next/font/google'
 import Providers from '@/components/Providers'
 import "react-loading-skeleton/dist/skeleton.css"
 import "simplebar-react/dist/simplebar.min.css"
 import { Toaster } from "@/components/ui/toaster"
 
-const inter = Inter({ subsets: ['latin'] })
+
 
 export const metadata = constructMetadata()
 
@@ -19,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en" className='light'>
       <Providers>
-        <body className={cn('min-h-screen font-sans antialiased grainy', inter.className)}>
+        <body className={'min-h-screen font-sans antialiased grainy'}>
           <Toaster />
           <Navbar />
           {children}
