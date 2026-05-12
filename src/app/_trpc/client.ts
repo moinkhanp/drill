24a31ffecab762745d3a0b56ecac84c493c9f520
moinkhanp <77323582+moinkhanp@users.noreply.github.com>
2026-05-12 11:40:45 +0530

    Update client.ts

--- src/app/_trpc/client.ts
+++ src/app/_trpc/client.ts
@@ -1,5 +1,5 @@
 import { createTRPCReact } from "@trpc/react-query";
-import type { AppRouter } from "@/server";
+import type { AppRouter } from "@/trpc";
 import { httpLink } from "@trpc/client";
 
 export const trpc = createTRPCReact<AppRouter>();
@@ -8,7 +8,7 @@ function getBaseUrl() {
   if (typeof window !== "undefined") return "";
 
   if (process.env.VERCEL_URL)
-    return `https://${process.env.VERCEL_URL}`;
+    return `https://${process.env.NEXT_PUBLIC_APP_VERCEL_URL}`;
 
   return `http://localhost:${process.env.PORT ?? 3000}`;
 }
