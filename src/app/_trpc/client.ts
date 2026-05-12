21bbd2ef2456611aa91b238616d40c55d16b4d32
moinkhanp <77323582+moinkhanp@users.noreply.github.com>
2026-05-12 12:08:29 +0530

    revert client.ts

--- src/app/_trpc/client.ts
+++ src/app/_trpc/client.ts
@@ -1,22 +1,4 @@
-import { createTRPCReact } from "@trpc/react-query";
-import type { AppRouter } from "@/trpc";
-import { httpLink } from "@trpc/client";
+import { AppRouter } from '@/trpc'
+import { createTRPCReact } from '@trpc/react-query'
 
-export const trpc = createTRPCReact<AppRouter>();
-
-function getBaseUrl() {
-  if (typeof window !== "undefined") return "";
-
-  if (process.env.VERCEL_URL)
-    return `https://${process.env.NEXT_PUBLIC_APP_VERCEL_URL}`;
-
-  return `http://localhost:${process.env.PORT ?? 3000}`;
-}
-
-export const trpcClient = trpc.createClient({
-  links: [
-    httpLink({
-      url: `${getBaseUrl()}/api/trpc`,
-    }),
-  ],
-});
+export const trpc = createTRPCReact<AppRouter>({})
