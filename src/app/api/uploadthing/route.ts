b223b40adc3eddda0b2947a90b0e21827abc9f29
moinkhanp <moinkhanp1999@gmail.com>
2026-08-01 10:56:22 +0530

    try to fix uploadthing

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -1,6 +1,9 @@
 import { createNextRouteHandler } from "uploadthing/next";
  
 import { ourFileRouter } from "./core";
+
+export const runtime = "nodejs";
+
  
 // Export routes for Next App Router
 export const { GET, POST } = createNextRouteHandler({
